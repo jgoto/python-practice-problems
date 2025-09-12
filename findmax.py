04ba4cd1af8413d1cf65bd447f60d52eca5ca559
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-09-11 20:52:15 -0400

    Added findmax.py and loopfactorial.py

---
+++ findmax.py
@@ -0,0 +1,10 @@
+# Find the maximum of three numbers
+
+a, b, c = 15, 72, 9
+
+if a > b and a > c:
+	print(a)
+elif b > a and b > c:
+	print(b)
+else:
+	print(c)
---
+++ loopfactorial.py
@@ -0,0 +1,9 @@
+# Compute factorial of a number using a loop
+
+num = 7
+fact = 1
+
+while(num > 0):
+	fact = fact * num
+	num -= 1
+print(fact)
